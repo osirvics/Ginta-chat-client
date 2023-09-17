3a7891a776d782a63013885f8896c83555b2f11a
osirvics <kedvic@gmail.com>
2023-09-17 20:55:56 +0100

    Initial Commit

---
+++ Wellprobe360/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wellprobe360
+//
+//  Created by Victor Edu on 17/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wellprobe360/Wellprobe360App.swift
@@ -0,0 +1,17 @@
+//
+//  Wellprobe360App.swift
+//  Wellprobe360
+//
+//  Created by Victor Edu on 17/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Wellprobe360App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
