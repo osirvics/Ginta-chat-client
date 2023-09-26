d122caafdb4c3e560c4182a796279b8db2ffcba9
osirvics <kedvic@gmail.com>
2023-09-26 23:45:22 +0100

    fix lint issues

--- Wellprobe360/Model/Conversation.swift
+++ Wellprobe360/Model/Conversation.swift
@@ -10,28 +10,15 @@ import Foundation
 
 // MARK: - Welcome
 struct ConversationResponse: Codable {
-    let directConversations: [DirectConversation]
+    let directConversations: [Conversation]
 
     enum CodingKeys: String, CodingKey {
         case directConversations = "direct_conversations"
     }
 }
 
-{
-  "event_type": "conversation_updated",
-  "payload": {
-    "direct_conversation_uuid": "9db9ad7d-75f9-42c1-86c6-f1c6a0333377",
-    "participant1_uuid": "e9e2faf4-31ee-495c-96b8-c2fd7b731aff",
-    "participant2_uuid": "e09c58b3-f1b5-4f50-9d0d-18eae402a950",
-    "last_message": "I don tire o",
-    "last_message_at": "2023-09-26T21:16:14.619827",
-    "created_at": "2023-09-25T17:12:56.284996",
-    "updated_at": "2023-09-26T21:16:14.619827"
-  }
-}
 
-// MARK: - DirectConversation
-struct DirectConversation: Codable, Identifiable {
+struct Conversation: Codable, Identifiable {
     var id: String {
         directConversationUUID
     }
@@ -54,5 +41,28 @@ struct DirectConversation: Codable, Identifiable {
         case participantFirstname = "participant_firstname"
         case participantLastname = "participant_lastname"
         case participantProfilePicture = "participant_profile_picture"
+    }
+}
+
+// MARK: - DirectConversation
+struct DirectConversation: Codable, Identifiable {
+    var id: String {
+        directConversationUUID
+    }
+    
+    
+    let directConversationUUID, participant1UUID, participant2UUID, lastMessage: String
+    let lastMessageAt, createdAt: String
+    let updatedAt : String?
+
+
+    enum CodingKeys: String, CodingKey {
+        case directConversationUUID = "direct_conversation_uuid"
+        case participant1UUID = "participant1_uuid"
+        case participant2UUID = "participant2_uuid"
+        case lastMessage = "last_message"
+        case lastMessageAt = "last_message_at"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
     }
 }
